94e7bf4f2266b6bf20dc229bc6d0e47e67bc06b3
Alice <aliciajuste@gmail.com>
2024-06-13 20:33:46 +0200

    Add only Given step

--- cypress/e2e/step_definitions/visit_istqb_test_page.js
+++ cypress/e2e/step_definitions/visit_istqb_test_page.js
@@ -5,7 +5,7 @@ import {
   } from "@badeball/cypress-cucumber-preprocessor";
   
 
-  Given("the user is at bootcamp qa login page", () => {
+  Given("the student visit istqb test page", () => {
     //Esta sentencia equivale a cy.visit("https://aliciajuste.github.io/istqb-online-test");
     //porque la url está definida en la variable baseUrl del fichero cypress-config.js
     cy.visit("/");
