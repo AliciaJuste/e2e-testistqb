3074e19c3743d141f9dc0d4d7a0a03ac5085a557
Alice <aliciajuste@gmail.com>
2024-06-28 22:12:38 +0200

    Update empty user message

--- cypress/e2e/features/see_welcome_message.feature
+++ cypress/e2e/features/see_welcome_message.feature
@@ -10,6 +10,6 @@ Then the student should see the message "Hi Alicia Jus, welcome to the ISTQB onl
 Scenario: Enter empty name
 
 Given the student is on the ISTQB exam page
-When the student enters the name ""
+When the student does not enter any name 
 And press start button
-Then the student should see the error message "The name cannot be empty"
+Then the student should see the message "The user name cannot be empty and must be a maximum of 10 characters long"
--- cypress/e2e/step_definitions/see_welcome_message.js
+++ cypress/e2e/step_definitions/see_welcome_message.js
@@ -5,22 +5,26 @@ import {
   } from "@badeball/cypress-cucumber-preprocessor";
   
 
-  Given("the student is on the ISTQB exam page", () => {
+  Given('the student is on the ISTQB exam page', () => {
     //Esta sentencia equivale a cy.visit("https://aliciajuste.github.io/istqb-online-test");
     //porque la url está definida en la variable baseUrl del fichero cypress-config.js
     cy.visit("/");
     
   });
 
-  When("the student enters the name {string}", (name) => {
+  When('the student enters the name {string}', (name) => {
     cy.get('[data-cy="student-name"]').clear();
     cy.get('[data-cy="student-name"]').type(name);
   });
 
-  When("press start button", () => {
+  When('press start button', () => {
     cy.get('[data-cy="register-button"]').click();
   });
 
-  Then("the student should see the message {string}", (welcomeMessage) => {
+  Then('the student should see the message {string}', (welcomeMessage) => {
     cy.get('[data-cy="welcome-message"]').should('have.text', welcomeMessage);
   });
+
+  When('the student does not enter any name', () => {
+    cy.get('[data-cy="student-name"]').clear();
+  })
