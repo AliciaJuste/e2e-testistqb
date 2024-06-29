7666eafa72baa957f9bc0a58c8283033c8812e28
Alice <aliciajuste@gmail.com>
2024-06-29 22:26:24 +0200

    Update See Answer feature

---
+++ cypress/e2e/features/see-answer.feature
@@ -0,0 +1,9 @@
+Feature: See answer
+
+Scenario: Select only 1 answer and clicks on the see answer button
+
+Given the student is on the ISTQB exam page
+When the student visits the test section
+And the student selects the answer "a" for the question "1"
+And the student clicks on the See answer button for question "1"
+Then the student should see the message "The correct answer is option c" for the question "1"
---
+++ cypress/e2e/step_definitions/see-answer.js
@@ -0,0 +1,31 @@
+import {
+    Given,
+    When,
+    Then,
+} from "@badeball/cypress-cucumber-preprocessor";
+
+
+Given('the student is on the ISTQB exam page', () => {
+    //Esta sentencia equivale a cy.visit("https://aliciajuste.github.io/istqb-online-test");
+    //porque la url está definida en la variable baseUrl del fichero cypress-config.js
+    cy.visit("/");
+});
+
+When('the student visits the test section', () => {
+    cy.visit("/test.html");
+    cy.url().should('include', '/test.html');
+});
+
+When('the student selects the answer {string} for the question {string}', (answerSelected, questionNumber) => {
+    // Seleccionar la respuesta para la pregunta
+    cy.get(`[data-cy="question-${questionNumber}-form"] [value="${answerSelected}"]`).click();
+});
+
+Then('the student clicks on the See answer button for question {string}', (questionNumber) => {
+    // Hacer clic en el botón de ver respuesta
+    cy.get(`[data-cy="question-${questionNumber}-See-Answer-Button"]`).click();
+});
+
+Then('the student should see the message {string} for the question {string}', (correctAnswerMessage, questionNumber) => {
+    cy.get(`[data-cy="response-Answer-${questionNumber}"]`).should('have.text', correctAnswerMessage);
+});
